b13c71f2366074cab3942cbd7999ed405f597a42
Es_lam <81473638+eslamredahassan@users.noreply.github.com>
2024-11-13 13:26:01 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
-java -version
-npm i install
+# start.sh
+sudo apt update
+sudo apt install -y openjdk-11-jdk  # or openjdk-17-jdk for Java 17
+
+java -version  # Check if Java is installed
+npm install
 node index

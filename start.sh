eec6a884dccb57be2612c2dd571f91149bf85062
Es_lam <81473638+eslamredahassan@users.noreply.github.com>
2024-11-13 13:29:42 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,18 @@
-# start.sh
-sudo apt update
-sudo apt install -y openjdk-11-jdk  # or openjdk-17-jdk for Java 17
+#!/bin/bash
 
-java -version  # Check if Java is installed
+# Download OpenJDK (Java 11) - you can replace with the JDK version you prefer
+wget https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.17%2B8/OpenJDK11U-jdk_x64_linux_hotspot_11.0.17_8.tar.gz
+
+# Extract the JDK
+tar -xvzf OpenJDK11U-jdk_x64_linux_hotspot_11.0.17_8.tar.gz
+
+# Set JAVA_HOME and update the PATH
+export JAVA_HOME=$(pwd)/jdk-11.0.17+8
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Verify Java installation
+java -version
+
+# Install dependencies and start Lavalink
 npm install
 node index
